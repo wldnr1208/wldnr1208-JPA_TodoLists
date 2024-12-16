ec18a10c1a2cfde542609f8784cefff53725fbdc
wldnr1208@naver.com <wldnr1208@naver.com>
2024-12-16 10:05:20 +0900

    "feat: 기본 세팅 및 gitignore test"

---
+++ src/main/java/com/example/jpatodolists/JpaTodoListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpatodolists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTodoListsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaTodoListsApplication.class, args);
+    }
+
+}
