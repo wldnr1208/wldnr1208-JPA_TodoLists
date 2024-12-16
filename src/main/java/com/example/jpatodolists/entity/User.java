f4d0e2464a9ef74f2d4fd3f1812065ad73444ff9
wldnr1208@naver.com <wldnr1208@naver.com>
2024-12-16 12:29:25 +0900

    feat: table created

---
+++ src/main/java/com/example/jpatodolists/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.jpatodolists.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // 비밀번호 필드 추가
+}
