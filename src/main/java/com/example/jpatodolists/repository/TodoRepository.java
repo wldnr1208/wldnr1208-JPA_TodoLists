40c5714e3ab13c62b68e6610dcf882a1b31013dd
wldnr1208@naver.com <wldnr1208@naver.com>
2024-12-18 21:22:32 +0900

    feat: paging todo get done

--- src/main/java/com/example/jpatodolists/repository/TodoRepository.java
+++ src/main/java/com/example/jpatodolists/repository/TodoRepository.java
@@ -1,7 +1,11 @@
 package com.example.jpatodolists.repository;
 
 import com.example.jpatodolists.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,4 +19,14 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
                 .orElseThrow(() ->
                         new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exist id = " + id));
     }
+    // 페이징을 위한 새로운 메서드 추가
+    @Query("SELECT t FROM Todo t " +
+            "JOIN FETCH t.user " +
+            "WHERE t.isDeleted = false " +
+            "AND t.user.id = :userId")
+    Page<Todo> findAllByUserIdWithPaging(@Param("userId") Long userId, Pageable pageable);
+
+    // 댓글 개수 조회 메서드 추가
+    @Query("SELECT COUNT(c) FROM Comment c WHERE c.todo.id = :todoId AND c.isDeleted = false")
+    Long countCommentsByTodoId(@Param("todoId") Long todoId);
 }
